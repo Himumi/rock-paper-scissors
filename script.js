7d513dc6f9cf65508c54b7303d521db1412f00fb
himumi <erliansyaha@gmail.com>
2023-11-13 21:31:40 +0900

    Create playRound() Function Based Pseudocode

--- script.js
+++ script.js
@@ -14,4 +14,35 @@ const getComputerChoice = () => {
     return result;
 };
 
-console.log(getComputerChoice());
+const playerSelection = prompt("Enter Rock, Paper, or Scissors", "");
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    let result;
+
+    if (playerSelection === computerSelection) {
+        result = "The result is a draw!";
+    } else if (playerSelection === "Rock") {
+        if (computerSelection === "Scissors") {
+            result = "You win! Rock beats Scissors.";
+        } else {
+            result = "You lose! Paper beats Rock.";
+        }
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            result = "You Win! Paper beats Rock.";
+        } else {
+            result = "You lose! Scissors beats Paper.";
+        }
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Paper") {
+            result = "You win! Scissors beats Paper.";
+        } else {
+            result = "You lose! Rock beats Scissors.";
+        }
+    }
+
+    return result;
+}
+
+console.log(playRound(playerSelection, computerSelection));
